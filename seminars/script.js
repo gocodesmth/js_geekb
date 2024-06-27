c766cd919c8709827b93dc38773ed98cf17e2055
Sherik <sherikjumayev1202@gmail.com>
2024-06-27 13:34:27 +0500

    complete task 2

--- seminars/script.js
+++ seminars/script.js
@@ -2,4 +2,26 @@
 
 console.log('Текст из script.js!');
 
+// Задание 2.
+
+// -1-
+let num = 123;
+console.log(num);
+
+// -2-
+let - a = 5;
+a = 7;
+console.log(a);
+
+// -3-
+let b = 1 + 2 + 3;
+console.log(b);
+
+// -4-
+
+// type 1 comment
+
+/*
+    type 2 comment
+*/
 
