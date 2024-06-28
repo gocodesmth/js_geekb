0fd95aa89fefd6e10b9870a92a7b42141e5bed7f
Sherik <sherikjumayev1202@gmail.com>
2024-06-29 00:09:45 +0500

    add alert, prompt, ternary operator

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 let result = 8;
 console.log(result); //2
+
+alert("Explicit content, parental revisory only!")
+
+prompt("How old are u", 18);
+
+if (8 > 9) {
+    console.log('smth wrong');
+} else {
+    console.log('everything ok');
+}
