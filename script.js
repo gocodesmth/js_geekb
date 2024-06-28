eeb5ef3b3184015e84d2b38abfbcd2a96daf7015
Sherik <sherikjumayev1202@gmail.com>
2024-06-29 00:48:41 +0500

    lection 3 complete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let result = 8;
 console.log(result); //2
 
+/*
 alert("Explicit content, parental revisory only!")
 
 prompt("How old are u", 18);
@@ -10,3 +11,19 @@ if (8 > 9) {
 } else {
     console.log('everything ok');
 }
+*/
+
+let userAnswer = prompt('Зимой и летом одним цветом');
+
+// if (userAnswer === '') {
+//     console.log('Empty');
+// } else if (userAnswer == 'елка') {
+//     alert('Верно!');
+// } else {
+//     alert('Не угадал');
+// }
+
+// (условие) ? ДА : НЕТ
+// (userAnswer == "елка") ? alert('Верно!') : alert('Не угадал');
+// or 
+alert((userAnswer.toLowerCase() == "елка") ? 'Верно!' : 'Не угадал');
