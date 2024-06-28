8ae2b7ee2137be2123550346fc56728c75b7b94a
Sherik <sherikjumayev1202@gmail.com>
2024-06-29 00:58:06 +0500

    update lection 3

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ let userAnswer = prompt('Зимой и летом одним цветом');
 // (userAnswer == "елка") ? alert('Верно!') : alert('Не угадал');
 // or 
 alert((userAnswer.toLowerCase() == "елка") ? 'Верно!' : 'Не угадал');
+
+// let pass = Number(prompt('Enter password'));
+// pass = Number(pass);
+// if (pass === 123) {
+//     alert('Go further')
+// } else {
+//     alert('Can`t go further');
+// }
+
+let pass = Number(prompt('Enter password'));
+alert((pass === 123) ? 'Go further' : 'Can`t go further');
