533603eeae59487c3676b1d5405b21b6297aff27
Sherik <sherikjumayev1202@gmail.com>
2024-07-12 18:45:01 +0500

    complete seminar3 task 2

--- seminar3/sript.js
+++ seminar3/sript.js
@@ -40,3 +40,33 @@
 // }
 // checkPositive();
 
+// Задание 2
+/*
+Задание 2 (тайминг 15 минут)
+1. Сделайте функцию, которая параметрами принимает 3 числа и
+выводит в консоль сумму этих чисел.
+2. С помощью созданной вами функции выведите в консоль сумму
+значений этих переменных.
+let param1 = 1;
+let param2 = 2;
+let param3 = 3;
+1. Дана фукнция
+function func(num = 5) {
+console.log(num * num);
+}
+Расскажите, каким будет результат каждого из вызовов функции.
+func(2); //4
+func(3); //9
+func();  //25
+*/
+
+// function getSum(num1, num2, num3) {
+//     num1 = Number(prompt("Enter number 1"));
+//     num2 = Number(prompt("Enter number 2"));
+//     num3 = Number(prompt("Enter number 3"));
+//     console.log(num1 + num2 + num3);
+// }
+// getSum();
+
+
+
